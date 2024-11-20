daa1bc8acf8919abedaaf36f55c66d5472f41357
RizEoR <dor.rost@bk.ru>
2024-11-20 18:57:33 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+class Program
+{
+    public int Sam(int a, int b)
+    {
+        return a + b;
+    }
+
+    private double Ymnno(double x, double y)
+    {
+        return x * y;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Введите число 1");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число 2:");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Program program = new Program();
+        int sum = program.Sam(a, b);
+        Console.WriteLine($"Сумма: {sum}");
+
+        Console.WriteLine("Введите число с плавающей точкой 1:");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите число с плавающей точкой 2:");
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        double p = program.Ymnno(x, y);
+        Console.WriteLine($"Произведение: {p}");
+    }
+}
